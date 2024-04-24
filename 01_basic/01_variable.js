a44c165cc5bbf9ee8106c7339e3d989d8ec61cbf
soumya ranjan sethy <soumya88sethy@outlook.com>
2024-04-24 10:02:05 +0530

    01_variable file added and some minner changes is test js file

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144513
+let accountEmail = "soumya@google.com"
+var acccountPassword = "1234567"
+accountCity = "Bhubaneswar"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "ranjan@google.com"
+acccountPassword = "8658981894"
+accountCity = "ghatgaon"
+
+console.table([accountEmail, acccountPassword, accountCity])
+
+/*
+Prefer not to use var
+beacause of issue in block scope functional scope
+*/
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -2,3 +2,9 @@ console.log("hello world")
 
 // this is edit comment in local pc by soumya ranjan sethy
 // file edited
+let x = 20
+let y = 50
+
+let z = x + y 
+
+console.log("the output is: " , z)
