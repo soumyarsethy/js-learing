a0f4373b810a999c0cef16db01fb57e4aad34083
soumya ranjan sethy <soumya88sethy@outlook.com>
2024-05-04 17:50:59 +0530

    pratice folder and file added

---
+++ pratice/pratice1.js
@@ -0,0 +1,16 @@
+// all prtice in js
+//profie of Soumya Ranjan sethy on twitter
+
+const profile = {
+  userName: "@soumya_sethy",
+  Name: "soumya ranjan sethy",
+  description: "live the every moment of life",
+  location: "mumbai, India",
+  dateOfBirth: "june 10, 1995",
+  joinDate: 2013,
+  following: 109,
+  followers: 12,
+  userPosts: 1,
+};
+
+console.log(profile);
