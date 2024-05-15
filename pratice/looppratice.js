93fe27ef4298c06a2368b2211a6a20bf489c7e52
soumya ranjan sethy <soumya88sethy@outlook.com>
2024-05-15 19:46:21 +0530

    loop pratice js file addded and code insertaion

---
+++ pratice/looppratice.js
@@ -0,0 +1,16 @@
+// print 1 to 1000 using while loop
+
+let i = 0;
+while(i <= 1000){
+     console.log("i = " , i);
+     i++;
+}
+
+// do while loop
+// print from 50 to 200
+
+let j = 50;
+do{
+    console.log("j = " ,j);
+    j++
+}while(j <= 1000);
--- shradha_class/class3loops&string.js
+++ shradha_class/class3loops&string.js
@@ -2,5 +2,49 @@
 
 //loops
 //loops are excute a piece of code again and again
+/*
+for
+while
+do-while
+*/
+//print 1 to 5
+// for(let count = 1; count <= 5; count++){
+//    console.log("soumya");
+// }
+// let a = 5;
+// let count = 1;
+// let c = count <= a ;
+// console.log(c);
+
+//to calculate sum of 1 to 5
+// let sum = 0;
+// for(let i=1; i<= 5; i++ ){
+//     sum = sum + i ; //sum = sum + 0
+// }
+
+// console.log("sum = " , sum);
+// console.log("loop is end");
+
+//infinite loop
+// for (let i=1; i>=0; i++) {
+//     console.log("i = " , i);
+// }
+
+//while loop
+
+let i = 1;
+while (i <= 5) {
+  console.log("soumya");
+  i++;
+}
+
+//do while loop
+let j = 1;
+do {
+  console.log("j= ", j);
+  j++;
+} while (j <= 5);
 
 
+//for of loop
+ 
